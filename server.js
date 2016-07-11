f976b96aaa31d56575107dfba0de2b8e6d836c70
László Székely-Tóth <notisracer@gmail.com>
2016-07-11 23:30:34 +0200

    implemented server-side querying for enhanced security

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ var http = require('http');
 var server = http.Server(app);
 var io = require('socket.io')(server);
 var serialport = require('serialport');
+var qs = require('qs');
+var request = require('request');
 
 var jsonContent;
 var portName = '/dev/ttyACM0';
@@ -24,6 +26,14 @@ var client = new StatsD(
 
 var pingUrl = "http://ipecho.net/plain";
 var defUrl = "http://racerzeroone.ddns.net/";
+var queryParams =
+{
+	u: 'public',
+	p: 'public',
+	db: 'mydb',
+	epoch: 'ms',
+	q: 'SELECT time,value FROM temp'
+};
 
 client.socket.on('error', function(error)
 {
@@ -31,6 +41,26 @@ client.socket.on('error', function(error)
 });
 
 
+
+app.get('/query', function(req, res)
+{
+	var url = 'http://localhost:8086/query?' + qs.stringify(queryParams);
+	request(
+	{
+		method:'GET',
+		uri: 'http://localhost:8086/query?',
+		qs: queryParams,
+		json: 'true'
+	}, 
+	function(error, response, body)
+	{
+		if (!error && response.statusCode == 200)
+		{
+			res.json(body);
+		}
+	});
+	//res.json("{\"results\": [{\"series\": [{\"name\": \"temp\",\"columns\": [\"time\",\"value\"],\"values\": [[1466536248710,27],[1466536259142,26.9],[1466645576000,27],[1466645606000,27],[1466645636000,27],[1466645666000,27]]}]}]}");
+});
 
 
 
@@ -58,12 +88,11 @@ ardu.on('data', function(data)
     }
 });
 
-http.get(pingUrl, function (response)
+http.get(pingUrl, function (res)
 {
-    response.on("data", function (data)
+    res.on("data", function (data)
     {
 		console.log("================================================================================");
-		console.log("    " + "Server running");
         console.log("    " + "ip:  " + data + ":8081");
 		console.log("    " + "url: " + defUrl);
 		console.log("================================================================================");
